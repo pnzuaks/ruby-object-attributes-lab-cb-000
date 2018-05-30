554754e8f6b0b888df13f7fe40c9cfcf769b0781
Patricia Nykaza <pnykaza@calstatela.edu>
2018-05-30 18:01:18 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,13 @@
 class Person
-  def initialize()
+  def initialize(breed)
+    @breed = breed 
   end
+  
+  def breed 
+    @breed
+  end 
+  
+  def breed=(new_breed)
+    @breed = new_breed
   
 end
